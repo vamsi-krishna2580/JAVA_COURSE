11601601b021a996c883448debaf77b0ae6c85f9
vamsi-krishna2580 <216156130+vamsi-krishna2580@users.noreply.github.com>
2026-06-20 18:47:12 +0530

    update main.java

--- Library_management_system/src/Main.java
+++ Library_management_system/src/Main.java
@@ -1,9 +1,34 @@
-import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = "";
-        sc.close();
+    public static void main(String args[]){
+
+        Book book1 = new Book(101, "maths", 2026, true, 12345, 100);
+        DVD dvd1 = new DVD(102,"matsh tutorials", 2025, true, 2, "Education");
+        Student s1 = new Student(101,"Vamsi", "CSE-AI", 2024);
+        Faculty f1 = new Faculty(101, "Krishna", "CSE");
+
+
+        Library lib = new Library();
+        lib.addItem(book1);
+        lib.addItem(dvd1);
+
+        lib.addMember(s1);
+        lib.addMember(f1);
+
+       // lib.displayAllItems();
+       // lib.displayAllMembers();
+
+        lib.issueItem(101, 101);
+        System.out.println();
+        lib.displayIssuedItems();
+        System.out.println();
+        lib.displayAllItems();
+        System.out.println();
+        lib.returnItem(101);
+        System.out.println();
+        lib.displayIssuedItems();
+        System.out.println();
+        lib.displayAllItems();
+        System.out.println();
+
     }
 }
